d3f54b5ffa0c00417ca73beca3fdca31e828cb4c
FacundoArce12 <133166798+FacundoArce12@users.noreply.github.com>
2024-01-04 12:42:50 -0300

    FirstCommit

---
+++ script.js
@@ -0,0 +1,29 @@
+botonDark = document.querySelector("#botonDark")
+audio_fondo = document.querySelector("#BocaSong")
+botonMusica = document.querySelector("#botonMusica")
+Titulo=  document.querySelector("#Título")
+
+
+botonMusica.addEventListener("click", () => {
+    if (audio_fondo.paused){
+        audio_fondo.load()
+        audio_fondo.play()
+    }else{
+        audio_fondo.pause()
+    }
+    botonMusica.classList.toggle("btn-light")
+   
+})
+
+var imagenes = ["img/FerdinandBostero.jpg", "img/Ferdinand.jpg",];
+var indiceImagen = 0;
+function cambiarImagen() {
+    // Obtener el elemento de la imagen por su ID
+    var miImagen = document.getElementById("miImagen");
+
+    // Cambiar la fuente de la imagen
+    miImagen.src = imagenes[indiceImagen];
+
+    // Incrementar el índice (circularmente)
+    indiceImagen = (indiceImagen + 1) % imagenes.length;
+}
